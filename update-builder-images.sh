93a9d5ef6d45c8830971c1e5046904fd536eb819
Jonas Karlman <jonas@kwiboo.se>
2026-03-03 21:00:44 +0000

    update builder images
    Signed-off-by: Jonas Karlman <jonas@kwiboo.se>

---
+++ update-builder-images.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e -x
+
+SCRIPTS_PATH=$(dirname $(readlink -f "${BASH_SOURCE[0]}"))
+
+pushd $SCRIPTS_PATH/minsys
+docker build --pull -f docker/Dockerfile -t minsys-builder:arm64 --target arm64 docker/
+docker build --pull -f docker/Dockerfile -t minsys-builder:armhf --target armhf docker/
+popd
+
+pushd $SCRIPTS_PATH/u-boot
+docker build --pull -f docker/Dockerfile -t u-boot-builder docker/
+popd
